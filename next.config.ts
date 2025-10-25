79ff9df05f4bae3a55eeead6d6499bfde82c5193
Nach <nach.vidan@gmail.com>
2025-10-25 22:47:22 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 
 import type {NextConfig} from 'next';
 
-const isProd = process.env.NODE_ENV === 'production'
-const repoName = 'tradelight'
-
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   },
